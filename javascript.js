87e7e57d7784809228b1e04c250a99d4b6e849e1
AnjaliKrishna <anjali2sheeja@gmail.com>
2024-10-30 14:54:00 +0530

    create function to play one round

--- javascript.js
+++ javascript.js
@@ -9,8 +9,44 @@ function getComputerChoice(){
         break;
     }    
 }
+
 function getHumanChoice(){
     let choice=prompt("Enter your choice  (Rock/Paper/Scissors) : ");
     return choice.toLowerCase();
 }
 
+let humanScore=0;
+let computerScore=0;
+
+const humanChoice=getHumanChoice();
+const computerChoice=getComputerChoice();
+
+function playRound(humanChoice,computerChoice){
+    
+    if(humanChoice==='rock' && computerChoice==='scissors'){
+        humanScore++;
+        console.log("You win!Rock beats Scissors");
+    }else if(humanChoice==='scissors' && computerChoice==='rock'){
+        computerScore++;
+        console.log("You lose!Rock beats Scissors");
+    }else if(humanChoice==='scissors' && computerChoice==='paper'){
+        humanScore++;
+        console.log("You win!Scissors beats Paper");
+    }else if(humanChoice==='paper' && computerChoice==='scissors'){
+        computerScore++;
+        console.log("You lose!Scissors beats Paper");
+    // }else if(humanChoice==='scissors' && computerChoice==='paper'){
+    //     humanScore++;
+    //     console.log("You win!Scissors beats Paper");
+    }else if(humanChoice==='rock' && computerChoice==='paper'){
+        computerScore++;
+        console.log("You lose!Paper beats Rock");
+    }else if(humanChoice==='paper' && computerChoice==='rock'){
+        humanScore++;
+        console.log("You win!Paper beats Rock");
+    } else{
+        console.log(`Oops!No points! ${humanChoice} same as ${computerChoice}`);
+    }
+
+}
+playRound(humanChoice,computerChoice);
